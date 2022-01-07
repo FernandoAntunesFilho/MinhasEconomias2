42986590351ef9510fe458db88455577c4024be5
Fernando Antunes <fernando.antunes1@gmail.com>
2022-01-06 21:01:38 -0300

    Criado o arquivo index e routes

---
+++ BackEnd/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { route } = require("express/lib/router");
+require("express-async-errors");
+const app = express();
+const router = require("./routes");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(router);
+app.use((err, req, res, next) => {
+  const erroMessage = err.message;
+  return res.status(400).json({ message: err.message });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ BackEnd/routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("MinhasEconomias2 no ar");
+});
+
+module.exports = router;
+
+//Este arquivo foi criado por último. Descobrir os proximos passos e dar andamento.
